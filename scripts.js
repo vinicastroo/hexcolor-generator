30a1d230e83ae41dadec26f1bf5a9ef5627ce838
vinicastro1206 <viniciusc.d.c@hotmail.com>
2019-12-08 23:45:19 -0300

    .

---
+++ scripts.js
@@ -0,0 +1,61 @@
+window.addEventListener("load", event => {
+  for (a = 0; a < 24; a++) {
+    let sHex = getHex();
+    let oCard = getCard(sHex);
+    oCard.style.background = sHex;
+    oCard.style.height = "232px";
+  }
+});
+
+function rand(iMin, iMax) {
+  return Math.round(Math.random() * (iMax - iMin)) + iMin;
+}
+
+function randomIndex(aHex) {
+  return rand(0, aHex.length - 1);
+}
+
+function getHex() {
+  const aHex = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F"
+  ];
+
+  let sHex = "#";
+  for (i = 0; i < 6; i++) {
+    let iIndex = randomIndex(aHex);
+    sHex += aHex[iIndex];
+  }
+
+  return sHex;
+}
+
+function getCard(sHex) {
+  let oCard = document.createElement("div");
+  oCard.setAttribute("class", "card");
+
+  let oCardContainer = document.createElement("div");
+  oCardContainer.setAttribute("class", "card-container");
+
+  let oCardContent = document.createElement("div");
+  oCardContent.setAttribute("class", "card-content");
+
+  oCardContent.innerText = sHex;
+  oCard.appendChild(oCardContainer);
+  oCardContainer.appendChild(oCardContent);
+  document.querySelector("#content").appendChild(oCard);
+  return oCard;
+}
